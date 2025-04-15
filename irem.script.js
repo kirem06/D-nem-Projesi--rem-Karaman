4e5cd9ba5391b673349ccf23340256290f6f1e09
kirem06 <krmnirem06@gmail.com>
2025-04-15 07:36:52 +0300

    5500nhatası giderildi
    5500 hatası giderildi

--- irem.script.js
+++ irem.script.js
@@ -1,22 +1,8 @@
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-  
-    const message = document.getElementById('message');
-    message.textContent = "Teşekkürler! Mesajınız iletildi.";
-    message.classList.remove('hidden');
-  
-    // Yeşil başarı mesajı stili
-    message.style.color = 'white';
-    message.style.backgroundColor = '#28a745';
-    message.style.padding = '10px';
-    message.style.borderRadius = '5px';
-  
-    this.reset();
-  
-    // 4 saniye sonra mesajı gizle
-    setTimeout(() => {
-      message.classList.add('hidden');
-    }, 4000);
-  });
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  document.getElementById('message').classList.remove('hidden');
+  this.reset();
+});
+
   
   
